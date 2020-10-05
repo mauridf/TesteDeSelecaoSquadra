11c74997cc4b24ab70d6e63d4f61c4f18db34f52
Maurício Dias de Carvalho Machado <mauridf@gmail.com>
2020-10-04 23:43:28 -0300

    Versão Final do Teste
    Última Versão do Teste compilado em 23h43min

---
+++ TesteSelecaoSquadra/Controllers/ShowMeTheCodeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace TesteSelecaoSquadra.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ShowMeTheCodeController:ControllerBase
+    {
+        private readonly ILogger<ShowMeTheCodeController> _logger;
+
+        public ShowMeTheCodeController(ILogger<ShowMeTheCodeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<ShowMeTheCode> Get()
+        {
+            return Enumerable.Range(1, 1).Select(index => new ShowMeTheCode
+            {
+               Url = "https://github.com/mauridf/TesteDeSelecaoSquadra/tree/main/TesteSelecaoSquadra"
+            })
+            .ToArray();
+        }
+    }
+}
